a7dac7c21a680ac440bbfe554f0113f9b6cfc44e
nitnux <43933828+nitnux@users.noreply.github.com>
2019-09-11 17:00:17 +0430

    logic framwork

--- tictactoe.py
+++ tictactoe.py
@@ -1,14 +1,20 @@
+from __future__ import print_function
 co=0
 def displayBoard(theBoard):
     count=0
     for c in theBoard.values():
         if count < 2:
-            print(c+'|')
+            print(c+'|',end='')
             count=count+1
         else:
-            print('/n')
-            count=0
-def playerinput(chance,theBoard):
+	    
+	    print("")
+	    while count > 0:
+		print('--',end='')
+            	count=count-1
+	    print("")
+
+def playerInput(chance,theBoard):
     x=input('Enter player{chance} choice  '.format(chance))
     y=input('Enter player{chance} posiion '.format(chance))
     updateBoard(x,y,theBoard)
@@ -20,53 +26,71 @@ def updateBoard(x,y,theBoard):
     dislayBoard(theBoard)
 def declareResult(y,chance,theBoard):
     if y == 'TL':
-        if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	else
+        if int(theBoard['TL'] )^int(theBoard['TM'] ) ^int(theBoard['TR'] ):
+		pass
+	elif int(theBoard['TL'] )^int(theBoard['TM'] ) ^int(theBoard['LL'] ):
+		pass
+	elif int(theBoard['TL'] )^int(theBoard['MM'] ) ^int(theBoard['LR'] ):
+		pass
+	else:
+		pass
         
-    if y == 'TM':
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	else
+    elif y == 'TM':
+	if int(theBoard['TM'] )^int(theBoard['TL'] ) ^int(theBoard['TR'] ):
+		pass
+	elif int(theBoard['TM'] )^int(theBoard['MM'] ) ^int(theBoard['LM'] ):
+		pass
+	else:
 	        pass
-    if y == 'TR':
- if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	else
+    elif y == 'TR':
+	if int(theBoard['TR'] )^int(theBoard['TM'] ) ^int(theBoard['TL'] ):
+		pass
+	elif int(theBoard['TR'] )^int(theBoard['MM'] ) ^int(theBoard['LL'] ):
+		pass
+	elif int(theBoard['TR'] )^int(theBoard['MR'] ) ^int(theBoard['LR'] ):
+		pass
+	else:
 	        pass
-    if y == 'ML':
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	else
+    elif y == 'ML':
+	if int(theBoard['ML'] )^int(theBoard['TL'] ) ^int(theBoard['LL'] ):
+		pass
+	elif int(theBoard['ML'] )^int(theBoard['MM'] ) ^int(theBoard['MR'] ):
+		pass
+	else:
         	pass
-    if y == 'MM':
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	else
+    elif y == 'MM':
+	if int(theBoard['MM'] )^int(theBoard['TL'] ) ^int(theBoard['LR'] ):
+		pass
+	elif int(theBoard['MM'] )^int(theBoard['TR'] ) ^int(theBoard['LL'] ):
+		pass
+	elif int(theBoard['MM'] )^int(theBoard['TM'] ) ^int(theBoard['LM'] ):
+		pass
+	elif int(theBoard['MM'] )^int(theBoard['ML'] ) ^int(theBoard['MR'] ):
+		pass
+	else:
         	pass
-    if y == 'MR':
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	else
+    elif y == 'MR':
+	if int(theBoard['MR'] )^int(theBoard['TR'] ) ^int(theBoard['LR'] ):
+		pass
+	elif int(theBoard['MR'] )^int(theBoard['MM'] ) ^int(theBoard['ML'] ):
+		pass
+	else:
         	pass
-    if y == 'LL':
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	else
+    elif y == 'LL':
+	if int(theBoard['LL'] )^int(theBoard['ML'] ) ^int(theBoard['TL'] ):
+		pass
+	elif int(theBoard['LL'] )^int(theBoard['MM'] ) ^int(theBoard['TR'] ):
+		pass
+	elif int(theBoard['LL'] )^int(theBoard['LM'] ) ^int(theBoard['LR'] ):
+		pass
+	else:
         	pass
-    if y == 'LM':
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	if int(theBoard['TL'] )^int(theBoard['TL'] ) ^int(theBoard['TL'] )
-	else
+    elif y == 'LM':
+	if int(theBoard['LM'] )^int(theBoard['MM'] ) ^int(theBoard['TM'] ):
+		pass
+	elif int(theBoard['LM'] )^int(theBoard['LL'] ) ^int(theBoard['LR'] ):
+		pass
+	else:
         	pass
     else :
         pass
@@ -83,6 +107,7 @@ if __name__ == '__main__' :
         else:
             count=count+1
             playerInput(count,theBoard)
+
 
 
 
